00f75ac27b702ed4c00e89b34136702f1e832d24
Emilio Bottoni <emilio.bottoni@edge.ufal.br>
2024-09-29 16:54:35 -0300

    Init Commit

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+	printf("[%u]Hello Emilio\n", k_cycle_get_32());
+
+	return 0;
+}
